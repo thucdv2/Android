8e3ab38f56d25ded7f1b1ba1b6dbf88d628606a7
dovanthuc09@gmail.com <dovanthuc09@gmail.com>
2020-04-06 13:31:49 +0700

    recycle view to show contact

---
+++ RecyclerView-Contact/app/src/main/java/com/sayan/LetterSectionedRecyclerView/CountriesModel.java
@@ -0,0 +1,15 @@
+package com.sayan.LetterSectionedRecyclerView;
+
+/**
+ * Created by Sayan Manna on 13-07-2017.
+ */
+
+public class CountriesModel {
+    public String name;
+    public int isSection;
+
+    public CountriesModel(String name, int isSection) {
+        this.name = name;
+        this.isSection = isSection;
+    }
+}
